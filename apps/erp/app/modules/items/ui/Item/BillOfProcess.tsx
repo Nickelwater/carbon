e34eb72dfee2eb53b3518fe3732bf03378b25f38
Sidwebworks <58144379+sidwebworks@users.noreply.github.com>
2026-03-03 01:42:59 +0530

    fix: disable steps on active method version

--- apps/erp/app/modules/items/ui/Item/BillOfProcess.tsx
+++ apps/erp/app/modules/items/ui/Item/BillOfProcess.tsx
@@ -2408,7 +2408,7 @@ function AttributesListItem({
           </div>
         </div>
       )}
-      {deleteModalDisclosure.isOpen && (
+      {!isDisabled && deleteModalDisclosure.isOpen && (
         <ConfirmDelete
           action={path.to.deleteMethodOperationStep(id)}
           isOpen={deleteModalDisclosure.isOpen}
@@ -2699,7 +2699,7 @@ function ParametersListItem({
           </div>
         </div>
       )}
-      {deleteModalDisclosure.isOpen && (
+      {!isDisabled && deleteModalDisclosure.isOpen && (
         <ConfirmDelete
           action={path.to.deleteMethodOperationParameter(id)}
           isOpen={deleteModalDisclosure.isOpen}
@@ -2932,7 +2932,7 @@ function ToolsListItem({
           </div>
         </div>
       )}
-      {deleteModalDisclosure.isOpen && (
+      {!isDisabled && deleteModalDisclosure.isOpen && (
         <ConfirmDelete
           action={path.to.deleteMethodOperationTool(id)}
           isOpen={deleteModalDisclosure.isOpen}
